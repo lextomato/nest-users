82ccedac84fb324b3619d9701d18c9f3828a6f97
Jhojany Uzcategui <externo.apside102@consorcio.cl>
2025-02-25 18:49:14 +0000

    Fix bcryptjs

--- src/common/utils/tools.ts
+++ src/common/utils/tools.ts
@@ -1,5 +1,8 @@
 import * as bcrypt from 'bcryptjs';
 
+// Configuración para usar un fallback en caso de que no haya un módulo de criptografía disponible
+bcrypt.setRandomFallback(require('crypto').randomBytes);
+
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt();
   return bcrypt.hash(password, salt);
